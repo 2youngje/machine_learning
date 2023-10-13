585b975079838b5ba0c4beb1da0343e250a29a34
youngje <dldudwp333@gmail.com>
2023-10-13 19:35:56 +0900

    Add_IGR

--- Decision_Tree/cal.py
+++ Decision_Tree/cal.py
@@ -13,6 +13,6 @@ IG_ratio_slope = IG_slope / IV_slope
 print(IG_ratio_slope)
 
 IG_elevation = 0.8774
-IV_elevation = -((2/7)*np.log2(2/7)+(3/7)*np.log2(3/7))
+IV_elevation = -((2/7)*np.log2(2/7)+(3/7)*np.log2(3/7)+2*(1/7)*np.log2(1/7))
 IG_ratio_elevation = IG_elevation / IV_elevation
 print(IG_ratio_elevation)
