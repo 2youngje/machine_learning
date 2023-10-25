e14277d3a2dac7aff72afd51532bb5ceca5be00b
youngje <dldudwp333@gmail.com>
2023-10-25 11:35:11 +0900

    GNB_change

--- bayes/GNB.py
+++ bayes/GNB.py
@@ -14,29 +14,29 @@ df_case.append(df['petal length (cm)'])
 df_case.append(df['petal width (cm)'])
 
 df_list = list()
-for i in range(12):
+for i in range(len(df_case)*len(df)):
     df_list.append(df_case[i % 4][df.target == (i // 4)])
 
 minmaxs = list()
-for i in range(4):
+for i in range(len(df_case)):
     minmaxs.append(df_case[i % 4].min())
     minmaxs.append(df_case[i % 4].max())
 
 spaces = list()
-for i in range(4):
+for i in range(len(df_case)):
     spaces.append(np.linspace(minmaxs[2*i], minmaxs[2*i+1], 1000))
 
-fig, axes = plt.subplots(4)
+fig, axes = plt.subplots(len(df_case))
 
-def JG(df, x, dot, n):
+def normallization(df, x, dot, n):
     mu = df.mean()
     sigma = df.std()
     y = (1 / np.sqrt(2 * np.pi * sigma ** 2)) * np.exp(-(x - mu) ** 2 / (2 * sigma ** 2))
     axes[n].scatter(dot, (1 / np.sqrt(2 * np.pi * sigma ** 2)) * np.exp(-(dot - mu) ** 2 / (2 * sigma ** 2)))
     return y
 
-for i in range(12):
-    y = JG(df_list[i], spaces[i % 4], df_case[i % 4].iloc[0], i % 4)
+for i in range(len(df_case)*len(df)):
+    y = normallization(df_list[i], spaces[i % 4], df_case[i % 4].iloc[0], i % 4)
     axes[i % 4].plot(spaces[i % 4], y)
 
 plt.show()
